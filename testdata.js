5a97d1b0ae9914503f52c8d4f38253a2460edabd
fola60 <afolabiadekanle@gmail.com>
2023-12-29 14:39:30 +0000

    --all

---
+++ testdata.js
@@ -0,0 +1,25 @@
+const {Client} = require('pg');
+
+const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password:"sarah2014",
+    database: "top5playerdata"
+})
+
+client.connect();
+
+client.query(`SELECT * FROM playerdata`,(err, res)=>{
+    // if(!err){
+    //     console.log(res.rows);
+    // } else {
+    //     console.log(err.message)
+    // }
+    for (r in res.rows){
+        console.log(r)
+    }
+    
+    client.end;
+})
+console.log("I cant believe that we made it this far")
