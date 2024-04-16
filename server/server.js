fa7aa0765173a689e4ed9a54e5aecbe1293f8295
fola60 <afolabiadekanle@gmail.com>
2024-04-16 17:45:26 +0100

    test

--- server/server.js
+++ server/server.js
@@ -38,22 +38,22 @@ io.on("connection", (socket) => {
     let dfBool = false;
     socket.on("send_id", (data) => {
         socket.join(socket.id);
-        socket.to(socket.id).emit("receive_id", data);
+        io.emit("receive_id", data);
         console.log("received id : " + data.id);
     });
     socket.on("send_league", (data) => { 
         socket.join(socket.id);
-        socket.to(socket.id).emit("received_league", data);
+        io.emit("received_league", data);
         console.log("received league : " + data);
     });
     socket.on("send_position", (data) => {
         socket.join(socket.id);
-        socket.to(socket.id).emit("received_position", data);
+        io.emit("received_position", data);
         console.log("received position", data);
     });
     socket.on("send_data_points",(data) => {
         socket.join(socket.id);
-        socket.to(socket.id).emit("receive_data_points",data);
+        io.emit("receive_data_points",data);
         dataPoints = data;
         console.log("received data points! :", dataPoints);
         mfBool = true;
@@ -62,7 +62,7 @@ io.on("connection", (socket) => {
     });
     socket.on("send-data_points_fw", (data) => {
         socket.join(socket.id);
-        socket.to(socket.id).emit("receive_data_points_fw",data);
+        io.emit("receive_data_points_fw",data);
         dataPointsFw = data;
         console.log("received data points Forward! :", dataPointsFw);
         fwBool = true;
@@ -71,7 +71,7 @@ io.on("connection", (socket) => {
     })
     socket.on("send-data_points_df", (data) => {
         socket.join(socket.id);
-        socket.to(socket.id).emit("receive_data_points_df",data);
+        io.emit("receive_data_points_df",data);
         dataPointsDf = data;
         console.log("received data points Defender! :", dataPointsDf);
         dfBool = true;
@@ -80,7 +80,7 @@ io.on("connection", (socket) => {
     })
     socket.on("send_players",(data) => {
         socket.join(socket.id);
-        socket.to(socket.id).emit("receive_players",data);
+        io.emit("receive_players",data);
         console.log("received player: " + data[0].player);
     })
     socket.on("send_player_data",(data) =>{
@@ -91,7 +91,7 @@ io.on("connection", (socket) => {
                 data[i].score = (data[i].mf_hvp * dataPoints.hvp) + (data[i].mf_da * dataPoints.da) + (data[i].mf_ca * dataPoints.ca) + (data[i].mf_gs * dataPoints.gs) + (data[i].mf_br  * dataPoints.br);
             }
             data.sort((a,b) => b.score - a.score);
-            socket.to(socket.id).emit("receive_sorted_player", data.slice(0,50));
+            io.emit("receive_sorted_player", data.slice(0,50));
             console.log(data);
             
             console.log("Player received!");
@@ -100,7 +100,7 @@ io.on("connection", (socket) => {
             fwBool = false;
         }
 
-        socket.to(socket.id).emit("receive_sorted_player", null);
+        io.emit("receive_sorted_player", null);
     });
     socket.on("send_player_data_fw", (data) => {
         socket.join(socket.id);
@@ -109,7 +109,7 @@ io.on("connection", (socket) => {
                 data[i].score = (data[i].fw_pr * dataPointsFw.pr) + (data[i].fw_fin * dataPointsFw.fin) + (data[i].fw_dr * dataPointsFw.dr) + (data[i].fw_cm * dataPointsFw.cm) + (data[i].fw_pass * dataPointsFw.pass);
             }
             data.sort((a,b) => b.score - a.score);
-            socket.to(socket.id).emit("receive_sorted_player_fw", data.slice(0,50));
+            io.emit("receive_sorted_player_fw", data.slice(0,50));
             
             console.log("Player received!");
             fwBool = false;
@@ -126,7 +126,7 @@ io.on("connection", (socket) => {
                 data[i].score = (data[i].df_aggr * dataPointsDf.aggr) + (data[i].df_bpa * dataPointsDf.bpa) + (data[i].df_passv * dataPointsDf.passv) + (data[i].df_aa * dataPointsDf.aa) + (data[i].df_da * dataPointsDf.da) + (data[i].df_pr * dataPointsDf.pr);
             }
             data.sort((a,b) => b.score - a.score);
-            socket.to(socket.id).emit("receive_sorted_player_df", data.slice(0,50));
+            io.emit("receive_sorted_player_df", data.slice(0,50));
             
             console.log("Player received!");
             fwBool = false;
